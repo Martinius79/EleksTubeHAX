9331608ebe1256f675236c0f931ec30da066b628
Clemens Sutor <clemenssutor@gmail.com>
2025-04-03 00:20:21 +0200

    Add modified RX8025T library to support second I2C bus for RTC chip of the NovelLifeSE - Added RTC usage for NovelLifeSE

--- EleksTubeHAX_pio/src/Clock.cpp
+++ EleksTubeHAX_pio/src/Clock.cpp
@@ -9,6 +9,9 @@ ThreeWire myWire(DS1302_IO, DS1302_SCLK, DS1302_CE); // IO, SCLK, CE
 RtcDS1302<ThreeWire> Rtc(myWire);
 void RtcBegin()
 {
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: RtcBegin() for DS1302 RTC entered.");
+#endif
   Rtc.Begin();
   // check if chip is connected and alive
   /*  TCS default value is 0x00 instead of 0x5C, but RTC seems to be working. Let's skip this check.
@@ -22,6 +25,27 @@ void RtcBegin()
   */
   if (!Rtc.IsDateTimeValid())
   {
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+    // Common Causes:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
     // Common Causes:
     //    1) first time you ran and the device wasn't running yet
     //    2) the battery on the device is low or even missing
@@ -43,13 +67,14 @@ void RtcBegin()
 uint32_t RtcGet()
 {
 #ifdef DEBUG_OUTPUT
-  Serial.println("RtcGet() for DS1302 RTC chip entered.");
+  Serial.println("DEBUG: RtcGet() for DS1302 RTC entered.");
+  Serial.println("DEBUG: Calling Rtc.GetDateTime()...");
 #endif
   RtcDateTime temptime;
   temptime = Rtc.GetDateTime();
   uint32_t returnvalue = temptime.Unix32Time();
 #ifdef DEBUG_OUTPUT
-  Serial.print("RtcGet() = ");
+  Serial.print("DEBUG: RtcGet() DS1302 returned: ");
   Serial.println(returnvalue);
 #endif
   return returnvalue;
@@ -58,30 +83,201 @@ uint32_t RtcGet()
 void RtcSet(uint32_t tt)
 {
 #ifdef DEBUG_OUTPUT
-  Serial.println("RtcSet() for DS1302 RTC chip entered.");
+  Serial.println("DEBUG: RtcSet() for DS1302 RTC entered.");
+  Serial.print("DEBUG: Setting DS1302 RTC to: ");
+  Serial.println(tt);
 #endif
   RtcDateTime temptime;
   temptime.InitWithUnix32Time(tt);
 #ifdef DEBUG_OUTPUT
-  Serial.print("Trying to set time now to: ");
-  Serial.println(temptime);
+  Serial.println("DEBUG: DS1302 RTC time set.");
 #endif
   Rtc.SetDateTime(temptime);
 }
-#else // for if defined(HARDWARE_SI_HAI_CLOCK) || defined(HARDWARE_IPSTUBE_CLOCK)
-// Alternatively for the DS1307 RTC chip
-#include <DS1307RTC.h>
+#elif defined(HARDWARE_NovelLife_SE_CLOCK) // for NovelLife_SE clone with R8025T RTC chip
+#include <RX8025.h>  // This header will now use Wire1 for I²C operations.
 
-void RtcBegin() {}
+RX8025 RTC;
+
+void RtcBegin()
+{
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: RtcBegin() for RX8025T RTC entered.");
+#endif
+  RTC.RX8025_init((uint32_t)RTC_SDA_PIN, (uint32_t)RTC_SCL_PIN); // setup second I2C for the RX8025T RTC chip  
+  Serial.println("RTC RX8025T initialized!");
+  delay(100);
+  return;
+}
+
+void RtcSet(uint32_t tt)
+{
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: RtcSet() for RX8025T RTC entered.");
+  Serial.print("DEBUG: Setting RX8025T RTC to: ");
+  Serial.println(tt);
+#endif
+
+// Convert epoch to individual fields.
+  uint8_t s  = second(tt);                  // seconds
+  uint8_t m  = minute(tt);                  // minutes
+  uint8_t h  = hour(tt);                    // hours (0-23)
+  uint8_t w  = weekday(tt);                 // weekday (1=Sunday, 2=Monday, ...)
+  uint8_t d  = day(tt);                     // day of month
+  uint8_t mh = month(tt);                   // month (1-12)
+  uint8_t y  = year(tt) - 2000;              // two-digit year (e.g. 21 for 2021)
+
+  // Optionally adjust weekday if needed by the RTC; for example, if RTC expects 0=Sunday.
+  // Here, we'll assume TimeLib's weekday() returns 1 for Sunday, which might be what your RTC expects.
+  
+  // Now call the setRtcTime function.
+  RTC.setRtcTime(s, m, h, w, d, mh, y);
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: RX8025T RTC time set.");
+#endif
+}
 
 uint32_t RtcGet()
 {
 #ifdef DEBUG_OUTPUT
-  Serial.println("RtcGet() for DS1307 RTC chip entered.");
+  Serial.println("DEBUG: RtcGet() for RX8025T RTC entered.");
 #endif
-  uint32_t returnvalue = RTC.get();
+  uint32_t returnvalue = RTC.getUnixtime(); // Get the RTC time
 #ifdef DEBUG_OUTPUT
-  Serial.print("RtcGet() = ");
+  Serial.print("DEBUG: RtcGet() RX8025T returned: ");
+  Serial.println(returnvalue);
+#endif
+  return returnvalue;
+}
+
+//#include <Wire.h>
+// #include <RTC_RX8025T.h>
+
+// void scanI2CBus() {
+//   byte error, address;
+//   int nDevices = 0;
+//   Serial.println("Scanning I2C bus...");
+
+//   for(address = 1; address < 127; address++ ) {
+//     Wire.beginTransmission(address);
+//     error = Wire.endTransmission();
+
+//     if (error == 0) {
+//       Serial.print("I2C device FOUND at 0x");
+//       if (address < 16)
+//         Serial.print("0");
+//       Serial.println(address, HEX);
+//       nDevices++;
+//     } else if (error == 4) {
+//       Serial.print("Unknown error at address 0x");
+//       if(address < 16)
+//         Serial.print("0");
+//       Serial.println(address, HEX);
+//     }
+//   }
+
+//   if (nDevices == 0)
+//     Serial.println("No I2C devices found.");
+//   else
+//     Serial.println("done.");
+// }
+
+// void RtcBegin()
+// {
+//   // scanI2CBus(); // Scan the I2C bus for devices
+//   //RX8025T initialization
+//   RTC_RX8025T.init();
+//   Serial.println("RTC init!");
+//   delay(100);
+// };
+
+// uint32_t RtcGet()
+// {
+// #ifdef DEBUG_OUTPUT
+//   Serial.println("RtcGet() for RX8025T RTC chip entered.");
+// #endif
+//   uint32_t returnvalue = RTC_RX8025T.get(); // Get the RTC time
+//   if (returnvalue == 0)
+//   {
+//     Serial.println("Error getting RTC time!");
+//   }
+//   else
+//   {
+// #ifdef DEBUG_OUTPUT
+//   Serial.print("RtcGet() = ");
+//   Serial.println(returnvalue);
+// #endif
+//   }
+//   return returnvalue;
+// };
+
+// void RtcSet(uint32_t tt)
+// {
+// #ifdef DEBUG_OUTPUT
+//   Serial.println("RtcSet() for RX8025T RTC chip entered."); 
+// #endif
+//   Serial.print("Trying to set time now to: ");
+//   Serial.println(tt);
+//   int ret = RTC_RX8025T.set(tt); // Set the RTC time
+//   if (ret != 0)
+//   {
+//     Serial.print("Error setting RTC time: ");
+//     Serial.println(ret);
+//   }else{
+//     Serial.println("RTC time set successfully!");
+//     Serial.print("RTCset() return value: ");
+//     Serial.println(ret);
+//   }
+// #ifdef DEBUG_OUTPUT
+//   Serial.print("RtcSet() = ");
+//   Serial.println(RTC_RX8025T.get()); // Get the RTC time again, because it may have changed in the meantime 
+// #endif
+//   delay(100);
+// }
+
+#else // for Elekstube and other clocks with DS3231 RTC chip
+// for if defined(HARDWARE_SI_HAI_CLOCK) || defined(HARDWARE_IPSTUBE_CLOCK)
+// Alternatively for the DS1307 RTC chip
+// #include <DS1307RTC.h>
+#include <RTClib.h>
+
+RTC_DS3231 RTC; // Funktioniert auch mit DS1307 oder PCF8523
+
+void RtcBegin() {  
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: RtcBegin() for DS3231 RTC entered.");
+#endif
+  if (!RTC.begin()) {
+    Serial.println("NO supported RTC found!");
+  }
+  else {
+    Serial.println("RTC found!");
+  }
+  Wire.begin();  // Standard: GPIO21 (SDA) & GPIO22 (SCL) for ESP32
+
+  Serial.println("Scanning I2C devices...");
+  for (uint8_t address = 1; address < 127; address++) {
+      Wire.beginTransmission(address);
+      if (Wire.endTransmission() == 0) {
+          Serial.print("I2C device FOUND at 0x");
+          Serial.println(address, HEX);
+      }
+      else {
+          Serial.print("I2C device NOT at 0x");
+          Serial.println(address, HEX);
+      }
+  }
+}
+
+uint32_t RtcGet()
+{
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: RtcGet() for DS3231 RTC entered.");
+#endif
+  DateTime now = RTC.now();   // convert to unix time
+  uint32_t returnvalue = now.unixtime();
+#ifdef DEBUG_OUTPUT
+  Serial.print("DEBUG: DS3231 RTC now.unixtime() = ");
   Serial.println(returnvalue);
 #endif
   return returnvalue;
@@ -90,20 +286,16 @@ uint32_t RtcGet()
 void RtcSet(uint32_t tt)
 {
 #ifdef DEBUG_OUTPUT
-  Serial.println("RtcSet() for DS1307 RTC chip entered.");
-  Serial.print("Trying to set time now to: ");
+  Serial.println("DEBUG: RtcSet() for DS3231 RTC entered.");
+  Serial.print("DEBUG: Attempting to set DS3231 RTC to: ");
   Serial.println(tt);
 #endif
 
-  bool re = RTC.set(tt);
-  if (!re)
-  {
-    Serial.println("RTC set failed!");
-  }
-  else
-  {
-    Serial.println("RTC set OK!");
-  }
+  DateTime timetoset(tt);   // convert to unix time
+  RTC.adjust(timetoset); // set the RTC time
+#ifdef DEBUG_OUTPUT
+  Serial.println("DEBUG: DS3231 RTC time updated.");
+#endif
 }
 #endif // end of RTC chip selection, else of (if defined(HARDWARE_SI_HAI_CLOCK) || defined(HARDWARE_IPSTUBE_CLOCK))
 
